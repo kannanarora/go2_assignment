6a171681b6b0adee7d7729972c0b3f2a6935bb8a
Mattias Tofte <mattiastofte@gmail.com>
2026-05-24 19:19:07 +1000

    Added boilerplate for not implemented

---
+++ go2_behaviours/go2_behaviours/behaviour_planner_node.py
@@ -0,0 +1,16 @@
+"""
+Behaviour planner placeholder.
+"""
+
+
+class BehaviourPlannerNode:
+    def __init__(self):
+        raise NotImplementedError('BehaviourPlannerNode is not implemented yet.')
+
+
+def main():
+    raise NotImplementedError('Behaviour planner entry point is not implemented yet.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ go2_behaviours/go2_behaviours/safety_monitor_node.py
@@ -0,0 +1,16 @@
+"""
+Safety monitor placeholder.
+"""
+
+
+class SafetyMonitorNode:
+    def __init__(self):
+        raise NotImplementedError('SafetyMonitorNode is not implemented yet.')
+
+
+def main():
+    raise NotImplementedError('Safety monitor entry point is not implemented yet.')
+
+
+if __name__ == '__main__':
+    main()
