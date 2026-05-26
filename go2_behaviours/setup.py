79ead5aa2e32cdd0bd388e15f34346094d8ba2d8
Allana Davies <s3720730@student.rmit.edu.au>
2026-05-26 14:15:57 +1000

    Added missing entry point

--- go2_behaviours/setup.py
+++ go2_behaviours/setup.py
@@ -27,6 +27,7 @@ setup(
             'behaviour_executor_node = go2_behaviours.behaviour_executor_node:main',
             'behaviour_planner_node = go2_behaviours.behaviour_planner_node:main',
             'sit_stand_loop_node = go2_behaviours.sit_stand_loop_node:main',
+            'wander_node = go2_behaviours.wander_node:main',
         ],
     },
 )
