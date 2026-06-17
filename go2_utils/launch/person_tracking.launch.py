34ed7f5769bfd6e77fc36e6a6d9a1986c2ea94b6
Mattias Tofte <mattiastofte@gmail.com>
2026-06-17 12:08:00 +1000

    Clean up code

--- go2_utils/launch/person_tracking.launch.py
+++ go2_utils/launch/person_tracking.launch.py
@@ -1,22 +1,14 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     share_dir = get_package_share_directory("go2_utils")
-    camera_source = LaunchConfiguration("camera_source")
-    video_client_camera_params = os.path.join(
-        share_dir,
-        "config",
-        "video_client_camera_params.yaml",
-    )
     gstreamer_camera_params = os.path.join(
         share_dir,
         "config",
@@ -35,23 +27,8 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            DeclareLaunchArgument(
-                "camera_source",
-                default_value="gstreamer",
-                description="Camera source: video_client or gstreamer",
-            ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(utils_launch),
-            ),
-            Node(
-                package="go2_utils",
-                executable="video_client_camera_node",
-                name="video_client_camera_node",
-                output="screen",
-                parameters=[video_client_camera_params],
-                condition=IfCondition(
-                    PythonExpression(["'", camera_source, "' == 'video_client'"])
-                ),
             ),
             Node(
                 package="go2_utils",
@@ -59,9 +36,6 @@ def generate_launch_description():
                 name="gstreamer_camera_node",
                 output="screen",
                 parameters=[gstreamer_camera_params],
-                condition=IfCondition(
-                    PythonExpression(["'", camera_source, "' == 'gstreamer'"])
-                ),
             ),
             Node(
                 package="go2_utils",
